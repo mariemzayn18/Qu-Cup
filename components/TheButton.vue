0d03f0537092e3caf7157853e17ce9519e38021f
mariemzayn18 <mariamzayn18@gmail.com>
2022-12-27 13:43:44 +0200

    clicked function for the button

--- components/TheButton.vue
+++ components/TheButton.vue
@@ -1,71 +1,73 @@
-
 <template>
-    <div class="btn">
-      <NuxtLink :to="route">
-        <button :style="buttonColor" :disabled="disabled">
-          {{ text }}
-        </button>
-      </NuxtLink>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      text: String,
-      route: {
-        type: [String],
-        default: "",
-      },
-      width: String,
-      height: String,
-      bgColor: {
-        type: [String],
-        default: "#d3d5d5",
-      },
-      textColor: {
-        type: [String],
-        default: "#6e1131",
-      },
-      disabled:{
-        type: [Boolean],
-        default: false ,
-      },
-      
+  <div class="btn">
+    <NuxtLink :to="route">
+      <button :style="buttonColor" :disabled="disabled" @click="buttonClicked">
+        {{ text }}
+      </button>
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String,
+    route: {
+      type: [String],
+      default: "",
     },
-    computed: {
-      Theme() {
-        return this.$store.state.Theme;
-      },
-  
-      buttonColor() {
-        return {
-          width: this.width,
-          height: this.height,
-          color:this.textColor,
-         " background-color": this.bgColor
-        };
-      },
+    width: String,
+    height: String,
+    bgColor: {
+      type: [String],
+      default: "#d3d5d5",
     },
-  };
-  </script>
-  
-  <style scoped>
-  @media only screen and (max-width: 990px) {
-    button {
-      width: 150px !important;
-      height: 45px !important;
-    }
-  }
+    textColor: {
+      type: [String],
+      default: "#6e1131",
+    },
+    disabled: {
+      type: [Boolean],
+      default: false,
+    },
+  },
+  computed: {
+    Theme() {
+      return this.$store.state.Theme;
+    },
+
+    buttonColor() {
+      return {
+        width: this.width,
+        height: this.height,
+        color: this.textColor,
+        " background-color": this.bgColor,
+      };
+    },
+  },
+
+  methods: {
+    buttonClicked() {
+      this.$emit("clicked");
+    },
+  },
+};
+</script>
+
+<style scoped>
+@media only screen and (max-width: 990px) {
   button {
-    width: 120px;
-    height: 50px;
-    border-radius: 10px;
-   
-    text-decoration: none;
-    font-size: 18px;
-    line-height: 21.94px;
- 
+    width: 150px !important;
+    height: 45px !important;
   }
-  </style>
-  
+}
+button {
+  width: 120px;
+  height: 50px;
+  border-radius: 10px;
+
+  text-decoration: none;
+  font-size: 18px;
+  line-height: 21.94px;
+}
+</style>
