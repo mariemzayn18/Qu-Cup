8ce36b7524b9ed5feacdd389dd53cbf0b8a06e85
Marim1611 <marimnaser813@gmail.com>
2022-12-27 13:28:37 +0200

    colors

--- store/index.js
+++ store/index.js
@@ -2,10 +2,9 @@ export const state = newFunction();
 function newFunction() {
   return () => ({
     Theme: {
-      color1: "#EB6440", //orange --> text
-      color2: "#497174", // turqouise --> borders
-      color3: "#D6E4E5", // baby blue --> (hovering/ underlining/ styling purposes)
-      color4: "#EFF5F5", //light blue --> BG
+      color1: "#6e1131", //purple
+      color2: "#d3d5d5", // gray
+      color3:"#d6e4e5", // white
       titleFontSize: "38px",
       subTitleFontSize: "24px",
       paragraphFontSize: "20px",
