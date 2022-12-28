b130aaa5fe3eb58e11802af150417baed472ad8e
nadeenay <70846138+nadeenay@users.noreply.github.com>
2022-12-28 23:31:39 +0200

    create, cancel, getall reservations without user validation check

--- src/routes/fan.js
+++ src/routes/fan.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 const router =Router()
-import {getAllMatches} from '../handlers/fan.js'
+import {getAllMatches,reserveMatch,cancelReservation,getAllUserReservations} from '../handlers/fan.js'
 
-// should put auth middleware
 // should put auth middleware
 
 router.get('/allmatches',getAllMatches)
+router.post('/reservation',reserveMatch)
+router.post('/cancelreservation',reserveMatch)
+router.get('/allreservation',getAllUserReservations)
 
 export default router
