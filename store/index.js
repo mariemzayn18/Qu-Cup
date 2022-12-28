c56415a6ba2e18b10ada043ce25a37696164b349
Marim1611 <marimnaser813@gmail.com>
2022-12-28 20:00:07 +0200

    sign up axios function

--- store/index.js
+++ store/index.js
@@ -47,12 +47,14 @@ export const actions = {
   },
   async signup({ commit }, user) {
     await axios
-      .post("http://localhost:8888/users/login", user)
+      .post("http://localhost:8888/users/signup", user)
+      console.log(user)
       .then((res) => {
         console.log(res) 
-              const user = res.data.user
+            console.log(res.data)
+            const user = res.data.user;
               const token = res.data.token
-              commit('auth_init', user,token)
+              commit('sign_up',user,token)
       })
       .catch((err) => {
         console.log(err);
@@ -73,6 +75,18 @@ export const actions = {
 //---------------------------------------- MUTATIONS ---------------------------------------------------
 export const mutations = {
   auth_init(state, user, token) {
+    state.token = token;
+    state.user.username = user.userName;
+    state.user.firstName = user.firstName;
+    state.user.lastName = user.lastName;
+    state.user.password = user.password;
+    state.user.email = user.email;
+    state.user.birthDate = user.birthDate;
+    state.user.nationality = user.nationality;
+    state.user.role = user.role;
+  },
+  sign_up(state, token) {
+    console.log("mutation signup")
     state.token = token;
     state.user.username = user.userName;
     state.user.firstName = user.firstName;
