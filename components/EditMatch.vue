34a9e6bb4711b0381bbfd0ac8292801d91f6dbb3
nadeenay <70846138+nadeenay@users.noreply.github.com>
2022-12-30 23:15:53 +0200

    edit

--- components/EditMatch.vue
+++ components/EditMatch.vue
@@ -1,127 +1,4 @@
 <template>
-    <v-container fluid>
-      <!-- <div class="form-container mx-7" > -->
-      <v-form
-        id="login"
-        class="form-container pa-10"
-        ref="form"
-        v-model="isValid"
-        action="/login"
-        method="post"
-      >
-       <v-row> <h3 class="my-4">Edit Match Details</h3></v-row>
-        <v-row>
-          <v-col>
-            <v-row>
-                <v-col>
-                    <v-select
-                    v-model="team1"
-                    :items="teams"
-                    label="Team 1"
-                    outlined
-                    ></v-select>
-                </v-col>
-                <v-col>
-                    <v-select
-                    v-model="team2"
-                    :items="teams"
-                    label="Team 2"
-                    outlined
-                    ></v-select>
-                </v-col>
-            </v-row>
-            <v-row>
-                <v-col>
-                    <v-select
-                    v-model="venue"
-                    :items="venues"
-                    label="Venue"
-                    outlined
-                    ></v-select>
-                </v-col>
-                <v-col>
-                    <v-text-field
-                    v-model="mainReferee"
-                    :rules="nameRules"
-                    name="mainReferee"
-                    label="Main Referee"
-                    autocomplete="off"
-                    outlined
-                    ></v-text-field>
-                </v-col>
-            </v-row>
-            <v-row>
-                <v-col>
-                    <v-text-field
-                    v-model="linesmen1"
-                    :rules="nameRules"
-                    name="linesmen1"
-                    label="First Linesmen"
-                    autocomplete="off"
-                    outlined
-                    ></v-text-field>
-                </v-col>
-                <v-col>
-                    <v-text-field
-                    v-model="linesmen2"
-                    :rules="nameRules"
-                    name="linesmen2"
-                    label="Second Linesmen"
-                    autocomplete="off"
-                    outlined
-                    ></v-text-field>
-                </v-col>
-            </v-row>
-            <v-col>
-                <v-alert
-                v-show="showTeamsAlert"
-                shaped
-                type="error"
-                >you can't choose the same team as team1 and team2</v-alert>
-                <v-alert
-                v-show="showSuccessAlert"
-                shaped
-                type="success"
-                >Match is updated successfully</v-alert>
-
-            </v-col>
-            <TheButton
-              @clicked="editMatch"
-              :disabled="!isValid"
-              text="Update"
-              bgColor="#6e1131"
-              textColor="#d3d5d5"
-            />
-          </v-col>
-          <v-col cols="3">
-                <p>Match Date</p>
-                <v-date-picker
-                v-model="date"
-                :min="new Date().toISOString().substr(0, 10)"
-                color="#6e1131 lighten-1"
-                header-color="#6e1131"
-                ></v-date-picker>
-            </v-col>
-            <v-col cols="3">
-                <p>Match Time</p>
-                <v-time-picker
-                v-model="time"
-                color="#6e1131"
-                 ></v-time-picker>
-            </v-col>
-        </v-row>
-      </v-form>
-    </v-container>
-  </template>
-  <script>
-  import TheButton from "./TheButton.vue";
-  import axios from "axios";
-  export default {
-    components: {
-      TheButton,
-    },
-    props: {
-    ID:String
   <v-container fluid>
     <!-- <div class="form-container mx-7" > -->
     <v-form
@@ -265,11 +142,11 @@ export default {
       console.log("new match");
       if (this.team1 == this.team2) {
         if(this.team1)
-              {
-                this.showTeamsAlert =true
-                this.showSuccessAlert=false
-                return
-              }
+        {
+        this.showTeamsAlert = true;
+        this.showSuccessAlert = false;
+        return;
+        }
       }
       this.showTeamsAlert = false;
       let teamOne = this.team1;
@@ -280,18 +157,18 @@ export default {
       let lineMan2 = this.linesmen2;
       let date = this.date + "T" + this.time;
       // TODO send id ad params
-      let ID = this.ID;
+      let matchID = this.ID;
       this.editMatchAction(
         { teamOne, teamTwo, matchVenue, date, mainReferee, lineMan1, lineMan2 },
-        ID
+        matchID
       );
       this.showAlert = true;
     },
-    async editMatchAction(match, ID) {
+    async editMatchAction(match, matchID) {
       console.log("%%%%%%#############################3");
       console.log(this.token);
       await axios
-        .patch(`http://localhost:8000/manager/match/${ID}`, match, {
+        .patch(`http://localhost:8000/manager/match/${matchID}`, match, {
           headers: {
             Authorization: `Bearer ${this.token}`,
           },
@@ -312,48 +189,6 @@ export default {
     token() {
       return this.$auth.$storage.getLocalStorage("token") || "";
     },
-    created(){
-        this.showSuccessAlert=false
-      }
-  };
-  </script>
-
-  <style scoped>
-  .v-text-field--outlined:deep(fieldset) {
-    border: 1px solid #e5e5e5;
-    border-radius: 8px;
-    text-decoration-color: white;
-  }
-  .form-container {
-    scroll-behavior: smooth;
-    border-radius: 2px;
-    background-color: #f1f5f6;
-    /* box-shadow: 0 0 20px rgba(163, 171, 185, 0.24); */
-  }
-  .date {
-    z-index: 10;
-  }
-  .name {
-    font-weight: 700;
-    font-size: 20px;
-  }
-  .un {
-    color: #475050;
-  }
-  .date{
-    width: 60px;
-    height:40px;
-    color:white;
-    border-radius:5px ;
-    background-color: #6e1131 ;
-    text-align: center;
-    font-size: 18px;
-  }
-  h3 {
-    color:#6e1131;
-  }
-  </style>
-
   },
   created() {
     this.showSuccessAlert = false;
